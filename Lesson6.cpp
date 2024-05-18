c30d314e284e7245c0af7a18ea06e98fda6d4155
Sander072 <sander072@mail.ru>
2024-05-18 17:38:05 +0300

    Добавьте файлы проекта.

---
+++ Lesson6.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+#define N 7
+
+int main()
+{
+	int array[N][N];
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			array[i][j] = i + j;
+			std::cout << array[i][j] << ' ';
+		}
+		std::cout << "\n";
+	}
+	std::cout << "\n";
+
+	int sum = 0;
+
+	for (int x = 0; x < N; x++)
+	{
+		sum += array[18 % N][x];
+	}
+	std::cout << sum << "\n";
+
+	return 0;
+}
